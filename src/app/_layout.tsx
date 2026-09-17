311f21b9ad23d026c650679cefba2845b79eed3c
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-09-17 16:35:33 -0300

    chore: configura NativeWind e Tailwind CSS no projeto

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
+import "../styles/global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
