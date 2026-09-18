acee3011201aea66bf81676209c1f35bbac6bd41
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-09-17 23:06:36 -0300

    chore: configura fontes com o nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        host: ["HostGrotesk-Regular"],
+        "host-light": ["HostGrotesk-Light"],
+        "host-medium": ["HostGrotesk-Medium"],
+        "host-semibold": ["HostGrotesk-SemiBold"],
+        "host-bold": ["HostGrotesk-Bold"],
+      },
+    },
   },
   plugins: [],
 };
