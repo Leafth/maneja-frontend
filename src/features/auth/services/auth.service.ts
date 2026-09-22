19d1ce83d9061677071211e798a23b4ff0c69997
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-09-22 11:16:27 -0300

    feat: adiciona endpoints da api

---
+++ src/features/auth/services/auth.service.ts
@@ -0,0 +1,81 @@
+import { api } from "@/infrastructure/api"
+
+import type {
+  AuthSession,
+  ForgotPassword,
+  Login,
+  Register,
+  RegisteredUser,
+  ResetPassword,
+  User,
+} from "../models";
+
+import type {
+  LoginResponseDTO,
+  RefreshTokenResponseDTO,
+  RegisterResponseDTO,
+  UserResponseDTO,
+} from "../models/dtos";
+
+import {
+  mapForgotPasswordToDTO,
+  mapLoginResponseToAuthSession,
+  mapLoginToDTO,
+  mapRefreshResponseToAuthSession,
+  mapRefreshTokenToDTO,
+  mapRegisterResponseToRegisteredUser,
+  mapRegisterToDTO,
+  mapResetPasswordToDTO,
+  mapUserResponseToUser,
+} from "../mapper";
+
+export const authService = {
+  async login(data: Login): Promise<AuthSession> {
+    const dto = mapLoginToDTO(data);
+
+    const response = await api.post<LoginResponseDTO>("/auth/login", dto);
+
+    return mapLoginResponseToAuthSession(response.data);
+  },
+
+  async register(data: Register): Promise<RegisteredUser> {
+    const dto = mapRegisterToDTO(data);
+
+    const response = await api.post<RegisterResponseDTO>("/auth/register", dto);
+
+    return mapRegisterResponseToRegisteredUser(response.data);
+  },
+
+  async me(): Promise<User> {
+    const response = await api.get<UserResponseDTO>("/auth/me");
+
+    return mapUserResponseToUser(response.data);
+  },
+
+  async refresh(refreshToken: string): Promise<AuthSession> {
+    const dto = mapRefreshTokenToDTO(refreshToken);
+
+    const response = await api.post<RefreshTokenResponseDTO>(
+      "/auth/refresh",
+      dto,
+    );
+
+    return mapRefreshResponseToAuthSession(response.data);
+  },
+
+  async logout(): Promise<void> {
+    await api.delete("/auth/logout");
+  },
+
+  async forgotPassword(data: ForgotPassword): Promise<void> {
+    const dto = mapForgotPasswordToDTO(data);
+
+    await api.post("/password/forgot", dto);
+  },
+
+  async resetPassword(data: ResetPassword): Promise<void> {
+    const dto = mapResetPasswordToDTO(data);
+
+    await api.patch("/password/reset", dto);
+  },
+};
