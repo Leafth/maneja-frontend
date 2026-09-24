5f94874b48e1f5f65e13d2980f599f74a7db976c
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-09-24 12:13:21 -0300

    chore: configura QueryClientProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,6 +6,10 @@ import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     "HostGrotesk-Light": require("../../assets/fonts/HostGrotesk-Light.ttf"),
@@ -26,9 +30,9 @@ export default function RootLayout() {
   }
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <StatusBar style="dark" />
       <Stack screenOptions={{ headerShown: false }} />
-    </>
+    </QueryClientProvider>
   );
 }
