457cfb83e5407c6168a53ac4353782346e5bc812
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-09-24 09:47:46 -0300

    feat: adiciona estrutura da home

---
+++ src/app/(app)/home.tsx
@@ -0,0 +1,11 @@
+import HomeView from "@/features/home/home-view";
+import { Stack } from "expo-router";
+
+export default function HomeRoute() {
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <HomeView />
+    </>
+  );
+}
