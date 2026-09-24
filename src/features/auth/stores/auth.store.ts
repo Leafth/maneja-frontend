419e4c9ce586c12ec40687ddd2460cb644b679dd
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-09-24 12:14:54 -0300

    feat: adiciona store global de autenticação com Zustand

---
+++ src/features/auth/stores/auth.store.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+
+import type { User } from "../models";
+
+type AuthStatus = "loading" | "authenticated" | "unauthenticated";
+
+interface AuthState {
+  user: User | null;
+  status: AuthStatus;
+
+  setUser: (user: User) => void;
+  setStatus: (status: AuthStatus) => void;
+
+  authenticate: (user: User) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  status: "loading",
+
+  setUser: (user) => {
+    set({ user });
+  },
+
+  setStatus: (status) => {
+    set({ status });
+  },
+
+  authenticate: (user) => {
+    set({
+      user,
+      status: "authenticated",
+    });
+  },
+
+  logout: () => {
+    set({
+      user: null,
+      status: "unauthenticated",
+    });
+  },
+}));
