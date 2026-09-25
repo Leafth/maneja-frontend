71e39a7e5f28274027dbf8bc8701037f9ae982df
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-09-24 22:41:23 -0300

    feat: adiciona implementação inicial da home

--- src/features/home/home-view.tsx
+++ src/features/home/home-view.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from "react-native";
+import { Button } from '@/shared/components/Button';
+import { Text, View } from 'react-native';
+import { useLogoutViewModel } from '../auth/viewmodels/use-logout.view-model';
+import { useMeViewModel } from '../auth/viewmodels/use-me.view-model';
 
 export default function HomeView() {
+  const { logout, isLoggingOut } = useLogoutViewModel();
+  const { user } = useMeViewModel();
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View className='flex gap-5'>
+      <Text className='text-6xl'>Home</Text>
+      <Text className='text-xl font-light'>
+        Bem vindo ao maneja {user?.name}!!
+      </Text>
+      <Button
+        onPress={logout}
+        isLoading={isLoggingOut}
+        className='bg-support-red'
+      >
+        Sair
+      </Button>
     </View>
   );
 }
